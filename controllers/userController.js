b7b77c7e369e9e1b1b1f4faa3b2cfa6243fbbe57
Miguel Escobar <mae2136@gmail.com>
2022-06-04 16:38:21 -0500

    Create User Controller

---
+++ controllers/userController.js
@@ -0,0 +1,63 @@
+const { Thought, User } = require('../models');
+
+
+module.exports = {
+    // getUsers
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // getSingleUser
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user found with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // createUser
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json('Created the user 🎉', user)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // updateUser
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // deleteUser
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this id!' })
+                    : res.json({ message: 'User deleted!' })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // addFriend
+
+    // deleteFriend
+};
